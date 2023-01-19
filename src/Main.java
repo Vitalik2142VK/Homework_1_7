b134928b7ab2e0960b7bac6dce749a7a8df90bdf
Vitalii <lennab@mail.ru>
2023-01-19 22:11:10 +0300

    Выполнены задания с 1 по 4.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,104 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+    public static void main(String[] args)
+    {
+        // Task1:
+        int[] arrInt = {1, 2, 3};
+        float[] arrFloat = {1.57f, 7.654f, 9.986f};
+        char[] arrChar = {'a', 'b', 'c'};
+
+        // Task2:
+        System.out.println("Task2:");
+        for (int i = 0; i < arrInt.length; i++)
+        {
+            if (arrInt[i] != arrInt[arrInt.length - 1])
+            {
+                System.out.print(arrInt[i] + ", ");
+            }
+            else
+            {
+                System.out.println(arrInt[i]);
+            }
+        }
+        for (int i = 0; i < arrFloat.length; i++)
+        {
+            if (arrFloat[i] != arrFloat[arrFloat.length - 1])
+            {
+                System.out.print(arrFloat[i] + ", ");
+            } else
+            {
+                System.out.println(arrFloat[i]);
+            }
+        }
+        for (int i = 0; i < arrChar.length; i++)
+        {
+            if (arrChar[i] != arrChar[arrChar.length - 1])
+            {
+                System.out.print(arrChar[i] + ", ");
+            } else
+            {
+                System.out.println(arrChar[i]);
+            }
+        }
+        System.out.println("##########");
+
+        //Task3:
+        System.out.println("Task3:");
+        for (int i = arrInt.length - 1; i > -1; i--)
+        {
+            if (arrInt[i] != arrInt[0])
+            {
+                System.out.print(arrInt[i] + ", ");
+            }
+            else
+            {
+                System.out.println(arrInt[i]);
+            }
+        }
+        for (int i = arrFloat.length - 1; i > -1; i--)
+        {
+            if (arrFloat[i] != arrFloat[0])
+            {
+                System.out.print(arrFloat[i] + ", ");
+            } else
+            {
+                System.out.println(arrFloat[i]);
+            }
+        }
+        for (int i = arrChar.length - 1; i > -1; i--)
+        {
+            if (arrChar[i] != arrChar[0])
+            {
+                System.out.print(arrChar[i] + ", ");
+            } else
+            {
+                System.out.println(arrChar[i]);
+            }
+        }
+        System.out.println("##########");
+
+        //Task4:
+        System.out.println("Task4:");
+        for (int i = 0; i < arrInt.length; i++)
+        {
+            if(arrInt[i] % 2 != 0)
+            {
+                arrInt[i] += 1;
+            }
+        }
+        for (int i = 0; i < arrInt.length; i++)
+        {
+            if (arrInt[i] != arrInt[arrInt.length - 1])
+            {
+                System.out.print(arrInt[i] + ", ");
+            }
+            else
+            {
+                System.out.println(arrInt[i]);
+            }
+        }
+        System.out.println("##########");
     }
 }
